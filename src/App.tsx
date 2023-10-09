1ee4d55f7fcc480759f76e39f03920726e53b6b6
Gurujyoti-polai <79266786+Gurujyoti-polai@users.noreply.github.com>
2023-10-09 14:57:16 +0530

    updated .env file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import RootLayout from "./pages/RootLayout";
+import Home from "./pages/Home";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +13,12 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path="*" element={<ErrorPage />} />
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
     </>
   );
 }
